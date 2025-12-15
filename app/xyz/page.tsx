be8ed4f42ac02b55f67ca0c0540736d814a0836a
Akash-xcoder <akash.xcoder@gmail.com>
2025-12-15 23:44:42 +0530

    6th 1

--- app/xyz/page.tsx
+++ app/xyz/page.tsx
@@ -6,6 +6,8 @@ import LandingTestimonials from "../components/LandingTestimonials";
 import WhyChooseUs from "../components/WhyChooseUs";
 import Footer from "../components/Footer";
 
+export const dynamic = 'force-dynamic';
+
 export default function XyzPage() {
   return (
     <div className="min-h-screen bg-background">
