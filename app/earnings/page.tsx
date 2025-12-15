4833d1e9a590f563958c7f2fe47b887c7f601d05
Akash-xcoder <akash.xcoder@gmail.com>
2025-12-15 23:31:28 +0530

    5th 8

--- app/earnings/page.tsx
+++ app/earnings/page.tsx
@@ -1,5 +1,7 @@
 import Earnings from '../components/Earnings';
 
+export const dynamic = 'force-dynamic';
+
 export default function EarningsPage() {
   return <Earnings />;
 }
--- app/watch-ads/page.tsx
+++ app/watch-ads/page.tsx
@@ -1,5 +1,7 @@
 import AdWatcher from "../components/AdWatcher";
 
+export const dynamic = 'force-dynamic';
+
 export default function WatchAdsPage() {
   return <AdWatcher />;
 }
